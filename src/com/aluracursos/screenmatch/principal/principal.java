ee0f679f6c179870d9da11ca0a573cc3742279b8
Jherson Medrano <mbjherson@gmail.com>
2025-06-13 15:29:41 -0500

    creacion del menu principal avance

---
+++ src/com/aluracursos/screenmatch/principal/principal.java
@@ -0,0 +1,14 @@
+package com.aluracursos.screenmatch.principal;
+
+import java.util.Scanner;
+
+public class principal {
+    public static void main(String[] args) {
+        Scanner scan =new Scanner(System.in);
+        System.out.println("Bienvenido a la herramienta de conversor de monedas");
+        System.out.println("Seleccione el tipo de moneda que desee ingresar");
+        System.out.println("1- Dolares");
+        System.out.println("2- Soles");
+        System.out.println("3- Pesos Chilenos");
+    }
+}
